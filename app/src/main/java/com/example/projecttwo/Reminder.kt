8da5d16823d27664375821469d850127b6218054
cacutler <116305038+cacutler@users.noreply.github.com>
2025-05-09 12:11:22 -0600

    Updated code styling

--- app/src/main/java/com/example/projecttwo/Reminder.kt
+++ app/src/main/java/com/example/projecttwo/Reminder.kt
@@ -3,11 +3,4 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.UUID
 @Entity
-data class Reminder(
-    @PrimaryKey val id: UUID,
-    val title: String,
-    val dueDate: String,
-    val location: String,
-    val completed: Boolean,
-    val notes: String
-)
+data class Reminder(@PrimaryKey val id: UUID, val title: String, val dueDate: String, val location: String, val completed: Boolean, val notes: String)
