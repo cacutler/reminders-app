eadc7b202b24418f6d4abd231382c9cadb7a2f2e
Alex Cutler <calexcutler@gmail.com>
2025-12-23 14:24:21 -0700

    Fixed the action bar overlay

--- app/src/main/java/com/example/projecttwo/MainActivity.kt
+++ app/src/main/java/com/example/projecttwo/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.projecttwo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Set up the toolbar
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
